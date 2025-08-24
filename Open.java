f4d034406a7753019475c2846390e2b012d69215
ayusht9900 <ayusht9900@gmail.com>
2025-08-24 23:31:13 +0530

    Second commit

--- Open.java
+++ Open.java
@@ -2,6 +2,10 @@
 public class Open{
 public static void main(String[] args) {
     System.out.println("Radhey Radhey");
+    System.out.println("Hello World");
+    System.out.println("Just trail");
+}
+}
 }
 
 }
